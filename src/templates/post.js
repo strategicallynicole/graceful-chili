7f4be050d9ff450918216a8e4e7d37846adf8967
Stackbit Code Editor <projects@stackbit.com>
2021-05-26 16:33:45 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -23,7 +23,7 @@ export default class Post extends React.Component {
               <article className="post post-full">
                 <header className="post-header inner-sm">
                   <h1 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
-                  <h2 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
+                  <h2 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.anothertitle', null)}</h2>
 
                   {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
                   <div className="post-subtitle">
