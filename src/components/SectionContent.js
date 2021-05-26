4d26a333b5627ce4366add5b2517c2b83b90c104
Stackbit Code Editor <projects@stackbit.com>
2021-05-26 16:31:34 +0000

    SectionContent.js: changed

--- src/components/SectionContent.js
+++ src/components/SectionContent.js
@@ -13,7 +13,7 @@ export default class SectionContent extends React.Component {
               <h2 className="block-title underline inner-sm">{_.get(section, 'title', null)}</h2>
               )}
                {_.get(section, 'anothertitle', null) && (
-              <h2 className="block-title underline inner-sm">{_.get(section, 'title2', null)}</h2>
+              <h2 className="block-title underline inner-sm">{_.get(section, 'anothertitle', null)}</h2>
               )}
               {_.get(section, 'image', null) && (
               <div className="block-image">
